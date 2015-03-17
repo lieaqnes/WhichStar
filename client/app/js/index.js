2dd23fb8ef523ecb4c90c02be5ba142e2104e5cd
Yin, Sophia(AWF) <xueyin@ebay.com>
2015-03-17 16:46:19 +0800

    twowaydatabinding

--- client/app/js/index.js
+++ client/app/js/index.js
@@ -1,7 +1,15 @@
 var app = angular.module('whichStarApp', []);
+
 app.controller('helloWorld', function($scope) {
   $scope.greeting = {
     text: 'Hello'
+  };
+});
+app.directive('hello', function() {
+  return {
+    restrict: 'E',
+    template: '<div>Hi everyone!</div>',
+    replace: true
   };
 });
 
