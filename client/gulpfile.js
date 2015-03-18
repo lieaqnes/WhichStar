59ea447b57378f30c3ac2b456fc43b542e281260
Yin, Sophia(AWF) <xueyin@ebay.com>
2015-03-18 18:26:12 +0800

    add gulp task to run server using brower-sync

--- client/gulpfile.js
+++ client/gulpfile.js
@@ -1,4 +1,9 @@
+'use strict';
+
 var gulp = require('gulp');
+var $ = require('gulp-load-plugins')();
+var browserSync = require('browser-sync');
+var reload = browserSync.reload;
 
 gulp.task('default', function() {
   // place code for your default task here
@@ -6,3 +11,26 @@ gulp.task('default', function() {
     console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
   });
 });
+gulp.task('serve', function() {
+  browserSync({
+    notify: false,
+    port: 9000,
+    server: {
+      baseDir: ['app'],
+      routes: {
+        '/bower_components': 'bower_components'
+      }
+    }
+  });
+
+  // watch for changes
+  gulp.watch([
+    'app/*.html',
+    'app/js/**/*.js',
+    'app/img/**/*'
+  ]).on('change', reload);
+
+  //gulp.watch('app/styles/**/*.<%= includeSass ? 'scss' : 'css' %>', ['styles']);
+  //gulp.watch('bower.json', ['wiredep', 'fonts']);
+});
+
