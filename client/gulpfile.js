b13247483646847e1c05a50683a0174e16e111c9
Yin, Sophia(AWF) <xueyin@ebay.com>
2015-03-19 20:21:48 +0800

    add gulp task for scss

--- client/gulpfile.js
+++ client/gulpfile.js
@@ -11,12 +11,30 @@ gulp.task('default', function() {
     console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
   });
 });
+
+gulp.task('styles', function() {
+  return gulp.src('app/styles/*.scss')
+    .pipe($.sourcemaps.init())
+    .pipe($.sass({
+      outputStyle: 'nested', // libsass doesn't support expanded yet
+      precision: 10,
+      includePaths: ['.'],
+      onError: console.error.bind(console, 'Sass error:')
+    }))
+    .pipe($.postcss([
+      require('autoprefixer-core')({browsers: ['last 1 version']})
+    ]))
+    .pipe($.sourcemaps.write())
+    .pipe(gulp.dest('.tmp/styles'))
+    .pipe(reload({stream: true}));
+});
+
 gulp.task('serve', function() {
   browserSync({
     notify: false,
     port: 9000,
     server: {
-      baseDir: ['app'],
+      baseDir: ['app', '.tmp'],
       routes: {
         '/bower_components': 'bower_components'
       }
