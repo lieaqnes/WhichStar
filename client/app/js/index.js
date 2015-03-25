649a8182d41013b86dcaf5f069d80e6707505a83
Yin, Sophia(AWF) <xueyin@ebay.com>
2015-03-25 16:57:39 +0800

    learn angular

--- client/app/js/index.js
+++ client/app/js/index.js
@@ -1,11 +1,14 @@
 var app = angular.module('whichStarApp', []);
 
-app.controller('helloWorld', function($scope) {
-  $scope.greeting = {
-    text: 'Hello',
-    A: 'A'
-  };
+app.controller('helloWorld', function($scope, $rootScope) {
+  $scope.greeting = 'World';
+  $rootScope.departure = 'Angular';
 });
+
+app.controller('ListCtrl', function($scope) {
+  $scope.names = ['Amy', 'Nicole', 'Leah', 'Jack'];
+});
+
 app.directive('hello', function() {
   return {
     restrict: 'E',
