67d497ba6808563e1799fe8cfdc9620fa94dbde4
Yin, Sophia(AWF) <xueyin@ebay.com>
2015-04-28 23:21:33 +0800

    use angularui-route

--- client/app/js/app.js
+++ client/app/js/app.js
@@ -1,17 +1,38 @@
-var app = angular.module('whichStarApp', ['ngRoute', 'testCtrls']);
+var app = angular.module('whichStarApp', ['ui.router', 'testCtrls']);
 
-app.config(function($routeProvider) {
-  $routeProvider.when('/hello', {
-    templateUrl: 'templates/hello.html',
-    controller: 'helloCtrl'
-  }).when('/list', {
-    templateUrl: 'templates/list.html',
-    controller: 'listCtrl'
-  }).when('/header', {
-    templateUrl: 'templates/header.html'
-    // controller: 'headerCtrl'
-  }).otherwise({
-    redirectTo: '/hello'
-  });
+app.run(function($rootScope, $state, $stateParams) {
+    $rootScope.$state = $state;
+    $rootScope.$stateParams = $stateParams;
+});
+
+app.config(function($stateProvider, $urlRouterProvider) {
+  $urlRouterProvider.otherwise('/index');
+  $stateProvider
+    .state('index', {
+      url: '/index',
+      views: {
+        '': {
+          templateUrl: 'templates/home.html'
+        },
+        'main@index': {
+          templateUrl: 'templates/hello.html'
+        },
+        'header@index': {
+          templateUrl: 'templates/header.html'
+        }
+      }
+    })
+  // $routeProvider.when('/hello', {
+  //   templateUrl: 'templates/hello.html',
+  //   controller: 'helloCtrl'
+  // }).when('/list', {
+  //   templateUrl: 'templates/list.html',
+  //   controller: 'listCtrl'
+  // }).when('/header', {
+  //   templateUrl: 'templates/header.html'
+  //   // controller: 'headerCtrl'
+  // }).otherwise({
+  //   redirectTo: '/hello'
+  // });
 });
 
