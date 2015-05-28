e9407cec0b45153cd4e324520ffd56dc573fbe27
Yin, Sophia(AWF) <xueyin@ebay.com>
2015-05-28 09:35:23 +0800

    modify

--- client/app/js/directives.js
+++ client/app/js/directives.js
@@ -1,11 +1,24 @@
 angular.module('myCarousel', [])
-.controller('CarouselController', ['$scope', function($scope, $element) {
+.controller('CarouselController', ['$scope', '$element', function($scope, $element) {
     var self = this,
         slides = self.slides = $scope.slides = [],
         currentIndex = -1;
+
+    self.setting = {
+        'width': $element[0].offsetWidth,
+        'height': $element[0].offsetHeight,
+        'slideWidth': 0,//set for default. will change after first slide insertion
+        'slideHeight': 0,//set for default. will change after first slide insertion
+        'scale': .9,
+        'speed': 500,
+        'autoPlay': true
+    };
     self.addSlide = function(slide, element) {
         slide.$element = element;
         slides.push(slide);
+        if (slides.length === 1) {
+            this.setting.slideWidth = element[0].offsetWidth;
+        }
     };
 
     $scope.name = 'test';
@@ -15,7 +28,8 @@ angular.module('myCarousel', [])
         restrict: 'E',
         templateUrl: '/templates/widget/carousel.html',
         controller: 'CarouselController',
-        transclude: true
+        transclude: true,
+        replace: true
     };
 })
 .directive('slide', function() {
